2581bf0e2b6a7a20cb2bd908160f815e5c82f565
Ralf Th. Pietsch <ratopi@abwesend.de>
2015-09-29 20:22:31 +0200

    fibonacci tail recursive version added.

--- src/fib.erl
+++ src/fib.erl
@@ -1,7 +1,7 @@
 % No tutorial without Fibonacci
 
 -module(fib).
--export([fib/1]).
+-export([fib/1, tail/1]).
 
 fib(0) ->
   1;
@@ -9,3 +9,15 @@ fib(1) ->
   1;
 fib(N) ->
   fib(N - 1) + fib(N - 2).
+
+tail(0) ->
+  1;
+tail(1) ->
+  1;
+tail(N) ->
+  tail(0, 1, N).
+
+tail(A, _, 0) ->
+  A;
+tail(A, B, N) ->
+  tail(B, A+B, N-1).
