b2e8767a335797bc601420e1d5513b169af3a7ca
Ralf Th. Pietsch <ratopi@abwesend.de>
2016-08-29 09:01:31 +0200

    example for generating random password...

---
+++ src/pwgen.erl
@@ -0,0 +1,10 @@
+-module(pwgen).
+-author("ratopi").
+
+-export([generate_password/1]).
+
+generate_password(N) ->
+	lists:map(
+		fun(_) ->
+			random:uniform(90) + $\s + 1 end, lists:seq(1, N)
+	).
