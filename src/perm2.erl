27e4c59131c97cc325bd7b2f41ca5590849344f3
Ralf Th. Pietsch <ratopi@abwesend.de>
2018-11-14 07:52:36 +0100

    send out permutation to pid or fun

--- src/perm2.erl
+++ src/perm2.erl
@@ -10,10 +10,42 @@
 -author("Ralf Thomas Pietsch <ratopi@abwesend.de>").
 
 %% API
--export([perm/1]).
+-export([perm/1, perm/2]).
 
 perm([]) ->
 	[[]];
 
 perm(List) ->
 	[[X | Y] || X <- List, Y <- perm(lists:delete(X, List))].
+
+
+
+perm(List, FunOrPid) ->
+	perm(List, List, FunOrPid).
+
+
+
+perm([], _, _) ->
+	ok;
+
+perm([X | Rest], List, FunOrPid) ->
+	Tails = perm(lists:delete(X, List)),
+	send_lists(X, Tails, FunOrPid),
+	perm(Rest, List, FunOrPid).
+
+
+
+send_lists(_, [], _) ->
+	ok;
+
+send_lists(X, [Tail | Rest], FunOrPid) ->
+	call([X | Tail], FunOrPid),
+	send_lists(X, Rest, FunOrPid).
+
+
+
+call(List, Pid) when is_pid(Pid) ->
+	Pid ! {permutation, List};
+
+call(List, Fun) when is_function(Fun, 1) ->
+	Fun(List).
